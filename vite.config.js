01b72f60bfe939ae306f05f33a6ba0212b921541
Serghei Degteariov <s.degteariov@adtelligent.com>
2024-09-06 20:09:13 +0300

    Install bootstrap 5

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import dotenv from 'dotenv';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+
+export default ({ mode }) => {
+    const config = dotenv.config();
+    let url = config.parsed.APP_URL;
+    url = (new URL(url));
+    url = url.hostname;
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: ['resources/scss/app.scss', 'resources/js/app.js'],
+                refresh: true,
+            }),
+        ],
+        server: {
+            hmr: {
+                host: url,
+            }
+        },
+    });
+}
