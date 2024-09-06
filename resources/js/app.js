54c5659cd9128f6c03433e6d9e00274d7471a4e4
Serghei Degteariov <s.degteariov@adtelligent.com>
2024-09-06 21:37:39 +0300

    Install theme

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,9 @@ import './bootstrap';
 import '../scss/app.scss';
 
 import * as bootstrap from 'bootstrap';
+
+import "./modules/sidebar";
+import "./modules/theme";
+import "./modules/feather";
+// Forms
+import "./modules/flatpickr";
